905353955a094c8896dcefa6dabf61fe6903487d
Brandon <brandongines@gmail.com>
2021-03-16 08:55:36 -0400

    form

--- src/components/RegisterUser/next-of-kin.js
+++ src/components/RegisterUser/next-of-kin.js
@@ -31,7 +31,7 @@ const NextOfKin = ({ step, setStep, user, setUser }) => {
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", setUser })
+      body: encode({ "form-name": "contact", ...setUser })
     });
     setStep(step + 1);
   };
