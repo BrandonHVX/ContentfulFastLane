37c2e0d5d0e2050c397133563652e8a427583b89
Brandon <brandongines@gmail.com>
2021-03-17 12:20:13 -0400

    form

--- src/components/RegisterUser/user-reducer.js
+++ src/components/RegisterUser/user-reducer.js
@@ -1,32 +1,33 @@
 export const DefaultUser = {
-  firstName: '',
-  lastName: '',
-  phoneNumber: '',
-  email: '',
+  firstName: "",
+  lastName: "",
+  phoneNumber: "",
+  email: "",
   nextOfKin: {
-    firstName: '',
-    lastName: '',
-    email: '',
-  },
-}
+    firstName: "",
+    lastName: "",
+    email: ""
+  }
+};
 
 export const UserReducer = (user, { type, payload }) => {
   switch (type) {
-    case 'UPDATE_PERSONAL_INFO':
+    case "UPDATE_PERSONAL_INFO":
       return {
         ...user,
-        ...payload,
-      }
-    case 'UPDATE_NEXTOFKIN_INFO':
+        ...payload
+      };
+    case "UPDATE_NEXTOFKIN_INFO":
       return {
         ...user,
         nextOfKin: {
+          ...user,
           ...user.nextOfKin,
-          ...payload,
-        },
-      }
+          ...payload
+        }
+      };
 
     default:
-      return user
+      return user;
   }
-}
+};
